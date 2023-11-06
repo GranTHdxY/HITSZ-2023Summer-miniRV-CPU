36f29e349c0271410156d4dc51326c2385c22823
X.Y Hu <100559891+GranTHdxY@users.noreply.github.com>
2023-11-06 16:03:18 +0800

    Add files via upload

---
+++ proj_single_cycle/proj_single_cycle.ip_user_files/sim_scripts/sim_scripts/cpuclk/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "C:/Xilinx/Vivado/2018.3/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "C:/Xilinx/Vivado/2018.3/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../proj_single_cycle.srcs/sources_1/ip/cpuclk/cpuclk_clk_wiz.v" \
+  "../../../../proj_single_cycle.srcs/sources_1/ip/cpuclk/cpuclk.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
